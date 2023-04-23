24e6d4548ce61c117c119983bd222848d759d4c0
Tomas Babej <tomas@proteinqure.com>
2023-04-23 01:17:39 -0400

    clwrap: Only look at a single GPU

--- src/clwrap.cpp
+++ src/clwrap.cpp
@@ -71,6 +71,7 @@ static vector<cl_device_id> getDeviceIDs(bool onlyGPU) {
   CHECK1(clGetPlatformIDs(16, platforms, (unsigned *) &nPlatforms));
   vector<cl_device_id> ret;
   cl_device_id devices[64];
+  nPlatforms = 1;
   for (int i = 0; i < nPlatforms; ++i) {
     unsigned n = 0;
     auto kind = onlyGPU ? CL_DEVICE_TYPE_GPU : CL_DEVICE_TYPE_ALL;
